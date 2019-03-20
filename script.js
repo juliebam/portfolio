8ee2ab661cc36713b8b7f887f3ed817c140c65a7
Julie <julie.bam1985@gmail.com>
2019-03-20 14:40:58 -0400

    committing latest changes

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ $(document).ready(function(){
 	$('.hamburger').click(function(){
         $(this).toggleClass('open');
         $('ul.navigator').toggleClass('show');
-	});
+    });
+    $('.landing').mouseover(function(){
+        console.log('the mouse is moving');
+        $('.triangle, .square, .circle, .circleStripes, .cross, .hollowTraingle, .triangleStripes, .hollowTraingle, .zigzag').toggleClass("move");
+    });
 });
+
+
+// $(".rotate").click(function(){
+//     $(this).toggleClass("down"); 
+// });
